f85e94983e039a535f48a179a3c4d6e632c11015
Carol Alexandru <alexandru@ifi.uzh.ch>
2023-09-10 18:21:33 +0900

    support user/pass for git repos

--- src/pages/CourseCreator.tsx
+++ src/pages/CourseCreator.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Heading, Input, InputGroup, InputLeftElement, Text, VStack } from '@chakra-ui/react'
+import { Button, Container, Flex, FormLabel, FormControl, Heading, Input, InputGroup, InputLeftElement, Text, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { GitHubIcon } from '../components/Icons'
@@ -6,25 +6,36 @@ import { useCreate } from '../components/Hooks'
 
 export default function CourseCreator() {
   const { mutate, isLoading }= useCreate()
-  const [repository, setRepository] = useState('')
+  const [course, setCourse] = useState(
+    {repository: '', repositoryUser: '', repositoryPassword: ''})
 
   return (
-      <VStack spacing={4} layerStyle='card' p={8} m={4}>
-        <Heading fontSize='3xl'>Let's get started!</Heading>
-        <Text color='gray.600' textAlign='center' lineHeight={1.5} w='md'>
-          To set up a new course on ACCESS, connect a Git repository to use as source for all your course data.
+  <Container>
+    <VStack spacing={4} layerStyle='card' p={8} m={4}>
+      <Heading fontSize='3xl'>Let's get started!</Heading>
+          <GitHubIcon />
+      <Text color='gray.600' lineHeight={1.5}>
+        To set up a new course on ACCESS, specify a Git repository to use as source for all your course data:
+      </Text>
+      <FormControl isRequired>
+        <FormLabel>
+          Course Repository URL (https):
+        </FormLabel>
+        <Input value={course.repository} onChange={e => setCourse({...course, repository: e.target.value})} type='text' />
+      </FormControl>
+        <Text color='gray.600' lineHeight={1.5}>
+          If the repository is private, provide a username and password. Note that you should not use your personal account credentials; instead, create a deploy token!
         </Text>
-        <Flex justifyContent="center" py={6}>
-          <InputGroup>
-            <InputLeftElement>
-              <GitHubIcon />
-            </InputLeftElement>
-            <Input w='container.md' value={repository} onChange={e => setRepository(e.target.value)} type='text' />
-          </InputGroup>
-        </Flex>
-        <Button variant='round' onClick={() => mutate({repository})} isLoading={isLoading}>
-          Submit
-        </Button>
-      </VStack>
+      <FormControl>
+        <FormLabel> Git username: </FormLabel>
+        <Input value={course.repositoryUser} onChange={e => setCourse({...course, repositoryUser: e.target.value})} type='text' />
+      </FormControl>
+      <FormControl>
+        <FormLabel> Git password: </FormLabel>
+        <Input value={course.repositoryPassword} onChange={e => setCourse({...course, repositoryPassword: e.target.value})} type='text' />
+      </FormControl>
+        <Button variant='round' onClick={() => mutate({...course})} isLoading={isLoading}> Submit </Button>
+    </VStack>
+  </Container>
   )
 }
