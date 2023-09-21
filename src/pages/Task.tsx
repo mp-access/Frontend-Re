e8de5a70a9e77363d20120ba2ae10a1c0a807ebe
Carol Alexandru <alexandru@ifi.uzh.ch>
2023-09-21 22:51:01 +0200

    contain task files in a folder when zipping

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -148,8 +148,8 @@ export default function Task() {
                 <ButtonGroup variant='ghost' size='sm' colorScheme='blackAlpha' pos='absolute' right={3} top={1}>
                   <IconButton icon={<Icon as={HiDownload} boxSize={5} />} aria-label='download' onClick={() => {
                     let zip = new JSZip()
-                    task.files.filter(file => !file.editable).forEach(file => zip.file(file.path, file.template))
-                    editableFiles.forEach(file => zip.file(file.path, getContent(file)))
+                    task.files.filter(file => !file.editable).forEach(file => zip.file(`${task.slug}${file.path}`, file.template))
+                    editableFiles.forEach(file => zip.file(`${task.slug}${file.path}`, getContent(file)))
                     zip.generateAsync({ type: 'blob' }).then(b => fileDownload(b, task.slug + '.zip'))
                   }} />
                 </ButtonGroup>
