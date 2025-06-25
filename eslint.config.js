608b686728244b83890d3b0004525ed9600186bc
Patric Salvisberg <patric.salvisberg@gmail.com>
2025-06-25 14:19:43 +0200

    es lint react hook rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
 import eslintConfigPrettier from "eslint-config-prettier"
+import reactHooksPlugin from "eslint-plugin-react-hooks"
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -8,5 +9,15 @@ export default tseslint.config(
   ...tseslint.configs.recommended,
   {
     ignores: ["dist/*"],
-  }
+    plugins: {
+      // register the plugin under its short name
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: {
+      // enforce exhaustive deps on hooks
+      "react-hooks/exhaustive-deps": "error",
+      // if you want also the basic rules-of-hooks check:
+      "react-hooks/rules-of-hooks": "error",
+    },
+  },
 )
