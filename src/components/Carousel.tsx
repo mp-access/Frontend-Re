0f15273e375ee737950fbb09bbaa654a95b849a8
Patric Salvisberg <patric.salvisberg@gmail.com>
2025-07-07 17:09:22 +0200

    use Editor instead of MD

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,14 +2,78 @@ import { Button, Divider, Flex, Heading } from "@chakra-ui/react"
 import "./Carousel.css"
 
 import React, { useEffect, useRef, useState } from "react"
-import { Markdown } from "./Panels"
+import { Editor } from "@monaco-editor/react"
+const code = `
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def scale(self, factor):
+        self.radius *= factor
+
+
+    def __repr__(self):
+        return f"Circle({self.radius})"`
+
+const codeLong = `
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def scale(self, factor):
+        self.radius *= factor
+
+    def __str__(self):
+        return f"A circle with radius {self.radius}"
+
+    def __repr__(self):
+        return f"Circle({self.radius})"
+
+class Square:
+    pass
+
+class Rect:
+    pass
+
+# when you've implemented all three, this function should work:
+def do():
+    shapes = [Square(10), Square(5), Rect(5, 100), Circle(25)]
+    print([shape.area() for shape in shapes])
+    print([type(shape) for shape in shapes])
+    for shape in shapes:
+        shape.scale(3)
+    print(shapes)            # uses __repr__
+    for shape in shapes:
+        print(shape)         # uses __str__
+
+# uncomment to try the function:
+#do()
+`
 
 const Slide = () => {
   return (
     <Flex direction={"column"} p={4}>
       <Heading fontSize="lg">{"{Student Username}"}</Heading>
       <Divider />
-      <Markdown children={"Hello World"}></Markdown>
+      <Editor
+        defaultValue={code}
+        options={{
+          readOnly: true,
+          minimap: { enabled: false },
+          scrollBeyondLastLine: false,
+        }}
+        language="python"
+      ></Editor>
     </Flex>
   )
 }
@@ -54,6 +118,7 @@ export const Carousel = () => {
       p={0}
       background={"transparent"}
       height={"full"}
+      maxH={390}
     >
       <Flex className="slider" width={"full"} borderRadius={"2xl"}>
         <Flex className="slides" ref={sliderRef}>
